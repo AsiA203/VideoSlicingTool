f69581b5162bd102f0278a0ff2f1cf5497f9d56c
AsiA203 <aselia.azimkanova@gmail.com>
2025-02-20 21:44:20 -0600

    pushing folders

---
+++ backend/src/main/java/com/example/video_slicing_tool/VideoSlicingToolApplication.java
@@ -0,0 +1,12 @@
+package com.example.video_slicing_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSlicingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSlicingToolApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/example/video_slicing_tool/VideoSlicingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_slicing_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSlicingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
