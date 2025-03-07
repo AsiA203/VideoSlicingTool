9168321a88e5a6ae0c6e68588d28f97503e71db1
AsiA203 <aselia.azimkanova@gmail.com>
2025-03-06 23:12:34 -0600

    - Created MetaDataExtractor.java - Updating pom.xml

--- backend/src/main/java/com/example/video_slicing_tool/Service/MetaDataExtractor.java
+++ backend/src/main/java/com/example/video_slicing_tool/Service/MetaDataExtractor.java
@@ -1,18 +1,18 @@
 package com.example.video_slicing_tool.Service;
 
-import com.github.kokorin.jaffree.ffprobe.FFprobe;
-
 import java.nio.file.Paths;
 
 public class MetaDataExtractor {
     protected String format;
     protected double duration;
     protected long bitRate;
-
+    
     String ffprobePath = "/usr/bin/ffprobe";
-    FFprobe ffprobe = new FFprobe(Paths.get(ffprobePath));
 
-    public void getMetaDataFromVideo(String path) {
 
+
+    public void getMetaDataFromVideo(String filePath) {
+        try {
+        }
     }
 }
